ae59e09331dd9cd521f43e9ce8a659e1164b5223
adamnemecek <adamnemecek@gmail.com>
2023-06-30 23:03:06 -0700

    formatting

--- tutte/misc/bstreambuf_test.cpp
+++ tutte/misc/bstreambuf_test.cpp
@@ -3,25 +3,29 @@
 
 using namespace std;
 
-int main(int argc, char* args[]) {
+int main(int argc, char *args[])
+{
   bstreambuf x;
 
   mpz_class v;
-  try {
+  try
+  {
     v = 12345678;
     v = v * v * v;
     cout << "V = " << v << endl;
-    
+
     x << v;
 
     bistream y(x);
-    
+
     y >> v;
-    
+
     cout << "V = " << v << endl;
-  } catch(runtime_error &e) {
+  }
+  catch (runtime_error &e)
+  {
     cout << "EXCEPTION CAUGHT!" << endl;
   }
-       
+
   return 0;
 }
--- util/convert.cpp
+++ util/convert.cpp
@@ -6,56 +6,79 @@
 
 using namespace std;
 
-int parse_number(unsigned int &pos, string const &str) {
+int parse_number(unsigned int &pos, string const &str)
+{
   int s = pos;
-  while(pos < str.length() && isdigit(str[pos])) {
+  while (pos < str.length() && isdigit(str[pos]))
+  {
     pos = pos + 1;
   }
-  istringstream ss(str.substr(s,pos));
+  istringstream ss(str.substr(s, pos));
   int r;
   ss >> r;
   return r;
 }
 
-void match(char c, unsigned int &pos, string const &str) {
-  if(pos >= str.length() || str[pos] != c) { throw runtime_error(string("syntax error -- expected '") + c + "', got '" + str[pos] + "'"); }
+void match(char c, unsigned int &pos, string const &str)
+{
+  if (pos >= str.length() || str[pos] != c)
+  {
+    throw runtime_error(string("syntax error -- expected '") + c + "', got '" + str[pos] + "'");
+  }
   ++pos;
 }
 
-vector<pair<unsigned int, unsigned int> > read_graph(std::istream &input) {
-  vector<pair<unsigned int, unsigned int> > edgelist;
+vector<pair<unsigned int, unsigned int>> read_graph(std::istream &input)
+{
+  vector<pair<unsigned int, unsigned int>> edgelist;
   unsigned int V = 0, pos = 0;
-    
-  bool firstTime=true;
+
+  bool firstTime = true;
   string in;
   input >> in;
 
-  while(pos < in.length()) {
-    if(!firstTime) { match(',',pos,in); }
-    firstTime=false;
+  while (pos < in.length())
+  {
+    if (!firstTime)
+    {
+      match(',', pos, in);
+    }
+    firstTime = false;
     // just keep on reading!
-    unsigned int tail = parse_number(pos,in);
-    match(',',pos,in); 
-    unsigned int head = parse_number(pos,in);
-    V = max(V,max(head,tail));
-    edgelist.push_back(std::make_pair(tail,head));
-  }  
+    unsigned int tail = parse_number(pos, in);
+    match(',', pos, in);
+    unsigned int head = parse_number(pos, in);
+    V = max(V, max(head, tail));
+    edgelist.push_back(std::make_pair(tail, head));
+  }
 
   return edgelist;
 }
 
-int main(int argc, char *argv[]) {
-  bool first_time=true;
-  while(!cin.eof()) {
-    if(!first_time) { cout << endl; }
-    first_time=false;
-    vector<pair<unsigned int, unsigned int> > g = read_graph(cin);
-    bool firstTime=true;
-    for(vector<pair<unsigned int, unsigned int> >::iterator i(g.begin());i!=g.end();++i) {
-      if(i->first == 0 && i->second == 0) { break; }
-      if(!firstTime) { cout << ","; }
-      firstTime=false;
-      cout << (i->first-1) << "--" << (i->second-1);
+int main(int argc, char *argv[])
+{
+  bool first_time = true;
+  while (!cin.eof())
+  {
+    if (!first_time)
+    {
+      cout << endl;
+    }
+    first_time = false;
+    vector<pair<unsigned int, unsigned int>> g = read_graph(cin);
+    bool firstTime = true;
+    for (vector<pair<unsigned int, unsigned int>>::iterator i(g.begin()); i != g.end(); ++i)
+    {
+      if (i->first == 0 && i->second == 0)
+      {
+        break;
+      }
+      if (!firstTime)
+      {
+        cout << ",";
+      }
+      firstTime = false;
+      cout << (i->first - 1) << "--" << (i->second - 1);
     }
   }
 }
